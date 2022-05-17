ef27ed14a7a33f526d316ed46b27e933d90efb05
yabby <yabby1997@gmail.com>
2022-05-17 10:57:09 +0900

    initial commit

---
+++ GradientLayerTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GradientLayerTest
+//
+//  Created by Seunghun Yang on 2022/05/17.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
